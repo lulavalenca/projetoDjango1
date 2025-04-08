22b051fc1a316348523de9e980c064862c3ab7aa
lulavalenca <lulavalencafilho@gmail.com>
2025-04-08 12:03:11 -0300

    aula render django

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={ 'name': 'Luiz Valença',})
 
 def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
