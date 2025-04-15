e39c54fe94da4a00469ca5a6525980920582623c
lulavalenca <lulavalencafilho@gmail.com>
2025-04-15 18:27:25 -0300

    adicionando nova rota

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context={ 'name': 'Luiz Valença',})
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={ 'name': 'Luiz Valença',})
