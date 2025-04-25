a7d0f5493262e0aef41a7a2f6d66181e1d5fe1ed
lulavalenca <lulavalencafilho@gmail.com>
2025-04-25 08:24:25 -0300

    adicionando Recipes na admin django

--- recipes/models.py
+++ recipes/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Category(models.Model):
         name = models.CharField(max_length=65)
-
+       
+        def __str__(self):
+           return self.name
+        
 
 
 class Recipe(models.Model):
@@ -28,5 +31,6 @@ class Recipe(models.Model):
         User, on_delete=models.SET_NULL, null=True
     )
       
-
+def __str__(self):
+    return self.title
 
